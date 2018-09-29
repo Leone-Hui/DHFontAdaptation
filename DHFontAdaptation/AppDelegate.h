249b05e3a7ba95c56fe259e5d99b856625d6626c
Leone-Hui <Leone-Hui@users.noreply.github.com>
2018-09-29 15:37:34 +0800

    Initial Commit

---
+++ DHFontAdaptation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DHFontAdaptation
+//
+//  Created by 惠大河 on 2018/9/29.
+//  Copyright © 2018年 惠大河. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DHFontAdaptation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DHFontAdaptation
+//
+//  Created by 惠大河 on 2018/9/29.
+//  Copyright © 2018年 惠大河. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
